1817fe098bdd104a6246ab282db3db8ee68fdda3
kanasob <kanako.sobajima@woodmac.com>
2015-11-15 18:17:46 +0000

    Update server.R

--- server.R
+++ server.R
@@ -1,28 +1,39 @@
 library(shiny)
 
-data <- read.csv("./Storm.csv")
+  data <- read.csv("Storm.csv", header = TRUE)
+  #Rename the column names as discribed in ui.R
+  colnames(data)[2] <- "Fatalities"
+  colnames(data)[3] <- "Injuries"
+  colnames(data)[4] <- "Total casualties"
+  colnames(data)[5] <- "Property damage"
+  colnames(data)[6] <- "Crop damage"
+  colnames(data)[7] <- "Total damage"
+  
 
-dataset <-data
+shinyServer(function(input, output) {
 
+  ## define dataset from input in Ui.R
+  dataset <- reactive({
+    subset(data, select = c(input$impact), 
+               subset = (Year >= input$year[1]& Year<=input$year[2]))
+  })
+  
+  ## evaluate input for output
+    output$plot <- renderPlot({
 
-shinyServer(
-  function(input, output) {
-    ## Option to download the dataset
-    output$downloadData <- downloadHandler(
-      filename = function() {paste(input$dataset, '.csv', sep='')},
-      content = function(file) {
-        write.csv(dataset(),file)
-        
-        output$inputWeather_Typevalue <- renderPrint({input$Weather_Type})
-        output$inputStatevalue <- renderPrint({input$State})
-        output$Total_Damage <- renderPrint ({Total_Damage(input$Weather_Type,input$State)})
-        output$Fatalities <- renderPrint({Fatalities(input$Weather_Type,input$State)})
-        output$Injuries <- renderPrint({Injuries(input$Weather_Type, input$State)})
-        output$Total_Casualties <- renderPrint({Total_Casualties(input$Weather_Type, input$State)})
-        output$Property_Damage <- renderPrint({Property_Damage(input$Weather_Type, input$State)})
-        output$Crop_Damage <- renderPrint({Crop_Damage(input$Weather_Type, input$State)})s
-      }
-    )
+      barplot(dataset()[,c(input$impact)],
+              main = paste(input$impact, "in", input$year[1], "-",input$year[2]),
+              ylab="the number of casualties/the value of damage (US$)",
+              xlab = "Year", 
+              col = input$color)
+  })
+    
+    output$summary <- renderPrint ({
+      ds <- dataset()
+      summary(ds)
+    })
+    
+})
   
 
   
