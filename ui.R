45c5c35c9723114708f359dd7ffc07025a2b22d3
kanasob <kanako.sobajima@woodmac.com>
2015-11-15 18:18:42 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,51 +1,38 @@
 library(shiny)
 
-data <- read.csv("./Storm.csv")
-
-dataset <-data
-
-# Define UI for application that draws a histogram
+# Define UI for dataset viewer application
 shinyUI(fluidPage(
   
-  titlePanel("Impact of Severe Weather on Public Health and Economy in USA"), 
+  # Application title
+  titlePanel("Impact of Severe Weather on Public Health and Economy in USA"),
   
+  #Set sidebar
   sidebarLayout(
     sidebarPanel(
-      selectInput('Weather_Type', 'Weather_Type', names (dataset), selected = "TORNADO"), 
-      selectInput('State', 'State', names (dataset), selected = "AL"),
-      downloadButton('downloadData', 'Download dataset')
-    )
-  )
+      selectInput("impact", 
+                  "Select impact type:", 
+                  choices =c("Fatalities","Injuries","Total casualties","Property damage","Crop damage","Total damage")),
+      
+      br(),
+      sliderInput("year", 
+                  label = "Select the range of years to display:",
+                  min = 1950, 
+                  max = 2012, 
+                  value = c(1950,2012), 
+                  format = "0000"),         
+      
+     br(),
+     radioButtons("color", "Select the colour of chart", choices=c("Green", "Blue", "Yellow"), selected = "Blue"),
+    
+     br(),
+     p("Data for the following impact types is available from 1993: Property damage, Crop damage, Total damage.")
     ),
+  
+  # set main panel
+  mainPanel(
+    
+    plotOutput("plot"), 
+    verbatimTextOutput("summary")
 
-    mainPanel(
-      h4('Instruction'),
-      
-      p('Storms and other severe weather events can cause both public health and economic problems.'),
-      p('This application provides the number of fatalities, injuries and casualties
-        and the total cost fordamage to properties and crops caused by weather across the USA
-        from 1950 to 2011.'),
-      p('select'),
-      code("Weather_Type"),
-      p('and'),
-      code("State"),
-      p('in the sidebar.'),
-      
-      h4 ('Your input'),
-      p('Weather Type:'), verbatimTextOutput("inputWeather_Typevalue"),
-      p('State:'), verbatimTextOutput("inputStatevalue"),
-      h4 ('Total Number of Fatalities'),
-      verbatimTextOutput("Fatalities"),
-      h4 ('Total Number of Injuries'),
-      verbatimTextOutput("Injuries"),
-      h4 ('Total Number of casualties'),
-      verbatimTextOutput("Total_Casualties"),
-      h4 ('Total cost of damages to properties ($US)'),
-      verbatimTextOutput("Property_Damage"),
-      h4 ('Total cost of damages to crop ($US)'),
-      verbatimTextOutput("Crop_Damage"),
-      h4 ('Total cost of damages to economy ($US)'),
-      verbatimTextOutput("Total_Damage")
-      
-    )
   )
+  )))
